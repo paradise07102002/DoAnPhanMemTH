68399acd641e7650dca560b8e765007cb2513d72
Tu <pottredz@gmail.com>
2023-10-07 17:20:21 +0700

    Tu lam Login

--- app/src/main/java/edu/huflit/myapplication/MyDatabase.java
+++ app/src/main/java/edu/huflit/myapplication/MyDatabase.java
@@ -1,7 +1,10 @@
 package edu.huflit.myapplication;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+
+import edu.huflit.myapplication.OOP.User;
 
 public class MyDatabase {
     SQLiteDatabase database;
@@ -12,4 +15,31 @@ public class MyDatabase {
         helper = new DBHelper(context);
         database = helper.getWritableDatabase();
     }
+    public boolean checkLogin(String username, String password)
+    {
+        String select = "SELECT * FROM " + DBHelper.TABLE_USER + " WHERE " + DBHelper.USERNAME_USER + " = " + "'" + username + "'" + " AND " + DBHelper.PASSWORD_USER + " = " + "'" + password + "'";
+        Cursor cursor = database.rawQuery(select, null);
+        if (cursor.moveToFirst() == false)
+        {
+            return false;//Ko đúng user or pass
+        }
+        else
+        {
+            return true;
+        }
+    }
+    public User checkRole(String username)
+    {
+        User user = new User();
+        String select = "SELECT * FROM " + DBHelper.TABLE_USER + " WHERE " + DBHelper.USERNAME_USER + " = " + "'" + username + "'";
+        Cursor cursor = database.rawQuery(select, null);
+        if (cursor != null)
+        {
+            int role_index = cursor.getColumnIndex(DBHelper.ROLE_USER);
+            cursor.moveToFirst();
+            user.setRole_user(cursor.getString(role_index));
+        }
+        return user;
+    }
+
 }
---
+++ app/src/main/java/edu/huflit/myapplication/fragments/Login2.java
@@ -0,0 +1,155 @@
+package edu.huflit.myapplication.fragments;
+
+import android.animation.ArgbEvaluator;
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.Fragment;
+
+import edu.huflit.myapplication.MyDatabase;
+import edu.huflit.myapplication.OOP.User;
+import edu.huflit.myapplication.R;
+
+public class Login2 extends Fragment {
+    View view;
+    private View background;
+    ImageView back;
+    Button btn_login;
+    TextView next_dang_ky;
+    MyDatabase database;
+    EditText username, password;
+//    ManHinhChinh manHinhChinh;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.login_2, container, false);
+       // manHinhChinh = (ManHinhChinh) getActivity();
+        database = new MyDatabase(getActivity());
+        anhXa();
+        setColorTextView();
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dangNhap();
+            }
+        });
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              //  manHinhChinh.gotoManHinhChinhAdmin();
+            }
+        });
+        next_dang_ky.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //manHinhChinh.gotoDangKy();
+            }
+        });
+        return view;
+    }
+    public void anhXa()
+    {
+        username = (EditText) view.findViewById(R.id.login_username2);
+        password = (EditText) view.findViewById(R.id.login_pass2);
+        btn_login = (Button) view.findViewById(R.id.login_login2);
+        back = (ImageView) view.findViewById(R.id.login2_back);
+        next_dang_ky = (TextView) view.findViewById(R.id.login2_next_dangky);
+    }
+    //CODE KHI ẤN BUTTON ĐĂNG NHẬP
+    public void dangNhap()
+    {
+        if (username.getText().toString().trim().length() == 0)
+        {
+            String p = "<font color='#FF0000'>Chưa nhập username</font>";
+            username.setHint(Html.fromHtml(p));
+            return;
+        }
+        if (password.getText().toString().trim().length() == 0)
+        {
+            String p = "<font color='#FF0000'>Chưa nhập password</font>";
+            password.setHint(Html.fromHtml(p));
+            return;
+        }
+        boolean check = database.checkLogin(username.getText().toString().trim(), password.getText().toString().trim());
+        if (check == false)
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setMessage("Tài khoản hoặc mật khẩu không chính xác!");
+            builder.setPositiveButton("Đóng", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.cancel();
+                }
+            });
+            builder.create().show();
+            return;
+        }
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("login", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("username", username.getText().toString().trim());
+        editor.putBoolean("is_login", true);
+        editor.apply();
+
+        User user = database.checkRole(username.getText().toString().trim());
+
+
+
+//        if (user.getRole_user().equals("admin"))
+//        {
+//            Intent intent = new Intent(getActivity(), ManHinhChinh.class);
+//            startActivity(intent);
+//        }
+//        else if (user.getRole_user().equals("user"))
+//        {
+//            Intent intent = new Intent(getActivity(), ManHinhChinh2.class);
+//            startActivity(intent);
+//        }
+//        else if (user.getRole_user().equals("librarian"))
+//        {
+//
+//        }
+    }
+    public void setColorTextView()
+    {
+        int[] colors = {
+                Color.parseColor("#99FFFF"),
+                Color.parseColor("#CC00FF"),
+                Color.parseColor("#CC99FF"),
+                Color.parseColor("#CCFFFF")
+        };
+        // Tạo một `ValueAnimator` để chuyển đổi màu sắc
+        ValueAnimator valueAnimator = ValueAnimator.ofObject(new ArgbEvaluator(), colors[0], colors[1], colors[2], colors[3]);
+        valueAnimator.setDuration(6000);
+        //lặp lại vô hạn
+        valueAnimator.setRepeatCount(ValueAnimator.INFINITE);
+        // Bắt đầu `ValueAnimator`
+        valueAnimator.start();
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(@NonNull ValueAnimator valueAnimator) {
+                // Lấy giá trị màu hiện tại
+                int color = (int) valueAnimator.getAnimatedValue();
+                // Đặt màu cho textview
+                btn_login.setBackgroundColor((int) valueAnimator.getAnimatedValue());
+            }
+        });
+    }}
+
